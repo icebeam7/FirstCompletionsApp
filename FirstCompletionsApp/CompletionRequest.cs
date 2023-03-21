96d07ae3dbb5ef9d03c376e6217c392ed3b9abab
Luis <tony_articuno@hotmail.com>
2023-03-21 05:02:27 +0100

    Initial version

---
+++ FirstCompletionsApp/CompletionRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCompletionsApp
+{
+	public class CompletionRequest
+	{
+		public string model { get; set; }
+		public string prompt { get; set; }
+		public float temperature { get; set; }
+		public int max_tokens { get; set; }
+		public int top_p { get; set; }
+		public int frequency_penalty { get; set; }
+		public int presence_penalty { get; set; }
+	}
+
+}
---
+++ FirstCompletionsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Net.Http.Headers;
+
+var endpoint = "https://api.openai.com/v1/completions";
+
+var data = "{\"model\": \"text-davinci-003\", \"prompt\": \"What is the language for the following text? Hola crayola!\", \"temperature\": 0.3, \"max_tokens\": 100, \"top_p\": 1, \"frequency_penalty\": 0, \"presence_penalty\": 0, \"logprobs\": 5 }";
+var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+var client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "---");
+
+var response = await client.PostAsync(endpoint, content);
+
+if (response.IsSuccessStatusCode)
+{
+	var result = await response.Content.ReadAsStringAsync();
+	Console.WriteLine(result);
+}
+
